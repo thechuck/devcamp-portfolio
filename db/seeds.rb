bb5a55f0df2bbca11078aca8cbeff11d0b8cd189
Charles Herb <thechuck111@gmail.com>
2018-06-26 16:56:16 -0700

    added seeds file with test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do |blog|
+  Blog.create!(
+    title:"My Bloig Post #{blog}",
+    body: "Bacon ipsum dolor amet capicola tenderloin porchetta, short loin filet mignon tongue sausage hamburger kevin. Tongue pig tail cow. Shoulder short ribs tail, jowl boudin beef meatloaf. Shoulder salami leberkas spare ribs venison, beef ribs pork short loin chicken tongue ground round rump pork loin tri-tip beef."
+  )
+end
+
+puts "10 blog posts created"
+
+5.times do |skill|
+  Skill.create!(
+    title: "skill number #{skill}",
+    percent_utilized: 20
+  )
+end
+
+puts "5 skills created"
+
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio #{portfolio_item}",
+    subtitle: "Ham pork belly hamburger #{portfolio_item}",
+    body: "Drumstick tail bacon tongue jowl, t-bone flank beef strip steak landjaeger bresaola ham hock chicken pork loin ground round.",
+    main_image: "http://via.placeholder.com/600x400",
+    thumb_image: "http://via.placeholder.com/350x200"
+  )
+end
+
+puts "9 portfolio items created"
